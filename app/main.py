ce8dbaf61b38bf5add30e80dee5e091f1eaf0969
root <root@DESKTOP-53DR3O3>
2024-09-16 22:52:58 -0500

    template de formulario

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app=FastAPI()
+##app.mount("static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def main():
-    return "f"
+
+@app.post("/")
+def main(request:Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html")
+
+    
+
+@app.get("/test/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str,item:str,param: str|None=None):
+    print ("params: ",request.query_params)
+    print ("params: ",request.body)
+    return templates.TemplateResponse(
+        request=request, name="test.html", context={"id": id}
+    )
+
 
 
 
