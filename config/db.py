b7ba97afcbdbfd9073a3cf3fb43fc5f90b76b8a5
Juan Manuel Valencia Zapata <jmvalenciz@protonmail.com>
2024-09-20 23:07:49 -0500

    Add database docker compose

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, MetaData
 DBuser="root"
 DBpassword="P4ssw0rd123*"
 
-engine = create_engine("mysql+pymysql://root:P4ssw0rd123@localhost:3306/mcmdonaciones")
+engine = create_engine("mysql+pymysql://user:passwor@localhost:3306/db")
 meta=MetaData()
 connection=engine.connect()
 
